59697a2c032efa66e990dccdec0779a06d976e2d
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-12-16 20:12:46 +0100

    Add the general styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
+import Testimonial from './components/Testimonials/Testimonial';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="bg-black text-red-500">
-      <p>Hello Nic</p>
-    </div>
+   <>
+   <Nav />
+   <About />
+   <Skills />
+   <Projects />
+   <Testimonial />
+   <Contact />
+   <Contact />
+   <Footer/>
+   </>
   );
 }
 
