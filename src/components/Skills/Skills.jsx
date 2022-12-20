afea02d28fec04d5644b96a367fe7fd7baafb4fc
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-12-20 19:59:57 +0100

    Add styling for the skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,73 +1,80 @@
 import React from 'react';
 import {BsPatchCheckFill} from 'react-icons/bs';
+import './Skills.css'
 
 const Skills = () => {
   return (
     <section id='#skills' >
-     <div className="container skillsContainer">
-          <h5>Technologies I've Used</h5>
+       <h5>Technologies I've Used</h5>
           <h2>Skills</h2>
-        <div className="skillsContent">
-          <div className="frontend">
-              <div className="frontendTools">
+     <div className="container skillsContainer">
+      <div className="frontend">
+              {/* <div className="frontendTools"> */}
                 <h3>Front-end Development</h3>
+                <div className="skillsContent"> 
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>HTML</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>CSS</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
-                 <h4>TAILWIND/SCSS</h4>
+                 <BsPatchCheckFill className='skillsIcon' />
+                 <h4>TAILWIND</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
+                 <h4>SCSS</h4>
+               </article>
+               <article className="skillsDetails">
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>JavaScript</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>React Js</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>Redux</h4>
                </article>
+               
               </div>
            </div>
            <div className="backend">
-             <div className="backendTools">
+             {/* <div className="backendTools"> */}
              <h3>Back-end Development</h3>
+             <div className="skillsContent"> 
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>Ruby</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>Ruby on Rails</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>Postgresql</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>RSpec</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>Swagger</h4>
                </article>
                <article className="skillsDetails">
-                 <BsPatchCheckFill />
+                 <BsPatchCheckFill className='skillsIcon' />
                  <h4>Git/GitHub</h4>
                </article>
              </div>
            </div>
         </div>
-      </div>
+      {/* </div> */}
     </section>
   )
 }
