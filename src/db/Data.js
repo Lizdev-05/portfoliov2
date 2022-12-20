94096679242bc3cf7dc49193b8954685dbb227ac
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-12-20 22:23:49 +0100

    Add data

---
+++ src/db/Data.js
@@ -0,0 +1,56 @@
+import IMG1 from './estate.webp'
+import IMG2 from './meal.webp'
+import IMG3 from './stock.webp'
+import IMG4 from './prudent.webp'
+import IMG4 from './bookstore2.webp'
+import IMG6 from './awesomebook.webp'
+
+const data = [
+    {
+        id: 1,
+        description: 'The Awesome estate web application displays a list of apartments that were provided by local data. The users can check out the featured and recent apartments. Also, the user can click the house to get the details of the selected house.',
+       image: 'images/estate.webp',
+        live: 'https://lizdev-awesome-estate.netlify.app/',
+        source: 'https://github.com/Lizdev-05/awesome-estate.git',
+      },
+      
+      {
+        id: 2,
+        description: 'The Meal DB web application displays a list of meals that were provided by an external API. The users can like a meal, leave some comments or make a reservation, in these cases an involvement API was used.',
+       image: './images/meal.webp',
+        live: 'https://lizdev-05.github.io/capstone-project/',
+        source: 'https://github.com/Lizdev-05/capstone-project.git',
+      },
+      
+      {
+        id: 3,
+        description: 'Stock metrics is a web(mobile-focused) app for checking the companies listed on the stock exchange and see their live metrics. Its data is consumed from the Financial modelling API',
+       image: './images/stock.webp',
+        live: 'https://lizdev-stock-metric.netlify.app/',
+        source: 'https://github.com/Lizdev-05/stock-market.git',
+      },
+
+      {
+        id: 4,
+        description: 'Prudent app is a Ruby on Rails mobile web application that allows users to keep track of their expenses and categories. have a list of transactions associated with a category, so that the user can see how much money they spent and on what.',
+       image: './images/prudent.webp',
+        live: 'https://prudent-app.onrender.com/',
+        source: 'https://github.com/Lizdev-05/Budget-app',
+      },
+      {
+        id: 5,
+      description: 'This is a Ruby on Rails app that allows user to create, delete, and read recipes created by them or the community, allowing the users to decide whether they should be public or not. Also, it allows users to create food records and generate a shopping list based on the missing food for their recipes.',
+     image: './images/recipe1.webp',
+      live: 'https://tranquil-crag-64773.herokuapp.com/',
+      source: 'https://github.com/Lizdev-05/Recipe-app',
+    },
+    
+    {
+      id: 6,
+      description: 'This project is focused on building is a simple website that displays a list of books that users have added.It allows you to add and remove books from that list also. The goal of this project is to build a simple multipage page responsive book website.',
+      image: './images/awesomebook.webp',
+      live: 'https://lizdev-05.github.io/Awesome-Books/',
+      source: 'https://github.com/Lizdev-05/Awesome-Books.git',
+    },
+    
+]
