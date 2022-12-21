888caae99b832141334a9b93b691f37ad59efaf3
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-12-21 12:39:11 +0100

    Add swipper js

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import recommendation from '../../db/Feedback';
-import './Testimonial.css'
+import './Testimonial.css';
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Testimonial = () => {
   return (
@@ -8,22 +17,29 @@ const Testimonial = () => {
       <h5>Feedback from peopele I have worked remotely with</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonialContainer">
+      <Swiper className="container testimonialContainer"
+          // install Swiper modules
+          modules={[ Pagination]}
+          spaceBetween={40}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
       {
         recommendation.map((item) => (
-          <article className="testimonial" key={item.id}>
+          <SwiperSlide className="testimonial" key={item.id}>
               <div className="avatar">
                 <img src={item.image} alt={item.name} />
               </div>
               <h5 className='peerName'>{item.name}</h5>
               <small className="review">{item.text}</small>
               <h5>{item.location}</h5>
-          </article>
+          </SwiperSlide>
         )) 
         
       }
        
-      </div>
+      </Swiper>
     </section>
   )
 }
--- src/db/Feedback.js
+++ src/db/Feedback.js
@@ -10,23 +10,24 @@ import IMG15 from './db-img/kenny.jpeg'
 
 
 const recommendation = [
+    
     {
       id: 1,
-      text: 'While at TIIDELAB, I saw that Elizabeth is an energetic individual and is skilled in her field. always willing to improve she will be a valuable asset to your organisation.',
-      image: IMG7,
-      name: 'Molema Lema',
-      location: 'Dubai, UAE'
-    },
-    {
-      id: 2,
       text: 'I worked together with Elizabeth at TIIDELab and I can say Elizabeth is very goal oriented as well as a team player, she was always easygoing. She was keenly devoted to the work, but being a real people person, she was also enjoyable to be around. Elizabeth demonstrated to be an exceptional software engineer. She was well organized and thrived under pressure.',
       image: IMG8,
       name: 'Damilola Ogungbenro',
       location: 'Ottawa, Canada'
     },
     {
+        id: 2,
+        text: 'While at TIIDELAB Initiative  Abuja Nigeria, I saw that Elizabeth is an energetic individual and is skilled in her field. always willing to improve she will be a valuable asset to your organisation.',
+        image: IMG7,
+        name: 'Molema Lema',
+        location: 'Dubai, UAE'
+      },
+    {
       id: 3,
-      text: 'I  have worked with Elizabeth at Micro-verse and her professionalism during meetings and punctuality are among her other outstanding traits something I appreciated. Elizabeth has strong problem-solving skills as well as excellent communication skills which are complementary to making her a great remote software developer. Any company that has a thing for a balance in project delivery, as well as a fun-minded person, would be happy to have her on their team, Being in a good setting she is a valuable member of any software development team.',
+      text: 'I  have worked with Elizabeth at Micro-verse and her professionalism during meetings and punctuality are among her other outstanding traits. Elizabeth has strong problem-solving skills as well as excellent communication skills which are complementary to making her a great remote software developer. Any company that loves balance in project delivery would be happy to have her on their team.',
       image: IMG9,
       name: 'Abdelmejid Oumer Ali',
       location: 'Addis Abbaba, Ethiopia'
@@ -47,14 +48,14 @@ const recommendation = [
     },
     {
       id: 6,
-      text: ' highly recommend Elizabeth as a Software Developer and would love to work together again with her. Elizabeth is amazing at her job! she knows her way around people, she is good with the students and colleagues, does whatever it takes to help colleagues, and gets things done. she makes sure that everyone is on the same page and focused on the main goal. I learned how to use sass from Elizabeth',
+      text: 'I highly recommend Elizabeth as a Software Developer and would love to work together again with her. Elizabeth is amazing at her job! she knows her way around people, she is good with the students and colleagues, does whatever it takes to help colleagues, and gets things done. she makes sure that everyone is on the same page and focused on the main goal. I learned how to use sass from Elizabeth',
       image: IMG12,
       name: 'Nicholas Chimaobi Emmanuel',
       location: 'Lagos, Nigeria'
     },
     {
       id: 7,
-      text: 'Elizabeth Ojesanmi is one of the smartest software developers I’ve ever met. I worked alongside her at TIIDELab Initiative as fellow software intern. During that time she showed her commitment and determination towards team building and making sure that technical projects comes into completion while meeting deadlines over. Elizabeth is a person who has exceptional leadership skills and she has used that skill to impact those around her and the success of the team and projects. She is also a genuinely kind, respectful, encouraging person.. I can recommend her for any position because I believe in her.',
+      text: 'Elizabeth is one of the smartest software developers I’ve ever met. I worked alongside her at TIIDELab. During that time she showed her commitment and determination towards team building and making sure that technical projects comes into completion while meeting deadlines over. Elizabeth is a person who has exceptional leadership skills and has used that skill to impact those around her and the success of the team and projects.',
       image: IMG13,
       name: 'Victor Okoroji',
       location: 'Lagos, Nigeria'
@@ -68,7 +69,7 @@ const recommendation = [
     },
     {
       id: 8,
-      text: 'lizabeth is one of the brightest software developers I’ve ever met. I worked alongside her at TiideLab where she was instrumental in building amazing projects, and in that time she proved her commitment and determination over and over again. Elizabeth is always challenging herself--she’s the type of person who reads documentation for fun, and her attention to detail is evident in all of the projects I’ve worked with her on. She’s also genuinely a kind, encouraging person, and kept me from falling into bug blues multiple times. I can recommend her with reservation.',
+      text: 'Elizabeth is one of the brightest software developers I’ve ever met. I worked alongside her at TiideLab where she was instrumental in building amazing projects. Elizabeth is always challenging herself--she’s the type of person who reads documentation for fun, and her attention to detail is evident in all of the projects I’ve worked with her on. She’s also genuinely a kind, encouraging person.',
       image: IMG15,
       name: 'Kehinde Onifade',
       location: 'Osun, Nigeria'
